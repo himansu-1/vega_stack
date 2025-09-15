ebf3fa5eeb312a6f774e0a271a413fb37f4970bc
himansu-1 <himansu111sekhar@gmail.com>
2025-09-15 17:17:11 +0530

    backend cord url inserted

--- server/server/settings.py
+++ server/server/settings.py
@@ -176,6 +176,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://vega-stack.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
