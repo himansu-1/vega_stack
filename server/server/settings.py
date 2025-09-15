c64d80b7d1db6fd712d25b670534068db8ce1af1
himansu-1 <himansu111sekhar@gmail.com>
2025-09-15 14:15:38 +0530

    setting url array changed

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&rdm!+az1loe9_uhido7w#9gacpqf1w-&lt@9a3+(jxv0mnry&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vega-stack.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
