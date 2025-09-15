75dcafb7afa7df413ac226a2463285eb058f6089
himansu-1 <himansu111sekhar@gmail.com>
2025-09-15 15:02:57 +0530

    next.js client side updates

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Production setup
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
